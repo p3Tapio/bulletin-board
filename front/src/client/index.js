e89a093440a415ec3e50fbb76eea1f4c8eed7230
p3tapio <p_tapio@yahoo.com>
2021-05-11 14:03:28 +0300

    react asennukset

---
+++ front/src/client/index.js
@@ -0,0 +1,18 @@
+import feathers from '@feathersjs/client';
+import rest from '@feathersjs/rest-client';
+import auth from '@feathersjs/authentication-client';
+import axios from 'axios';
+
+const feathersClient = feathers();
+const restClient = rest(process.env.REACT_APP_API_URL);
+
+feathersClient.configure(restClient.axios(axios));
+feathersClient.configure(feathers.authentication());
+feathersClient.configure(
+  auth({
+    storage: window.sessionStorage,
+    storageKey: 'feathers-react-jwt',
+  })
+);
+
+export default feathersClient;
