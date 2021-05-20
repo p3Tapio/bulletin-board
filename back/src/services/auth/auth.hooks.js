766c62babef11094e476721efbf451b92b7a8d70
p3tapio <p_tapio@yahoo.com>
2021-05-20 20:43:30 +0300

    bullet populate

--- back/src/services/auth/auth.hooks.js
+++ back/src/services/auth/auth.hooks.js
@@ -17,23 +17,19 @@ module.exports = {
     find: [],
     get: [],
     create: [
-      async (context) => {
-        const User = context.app.service('users').Model;
-        // console.log(`context.data`, context.result.user._id);
-        const id = context.result.user._id;
-        const populated = await User.findOne({
-          _id: id,
-        }).populate('bulletins');
-
+      (context) => {
+        //   const User = context.app.service('users').Model;
+        //   const id = context.result.user._id;
+        //   const populated = await User.findOne({
+        //     _id: id,
+        //   }).populate('bulletins');
+        //   if (context.result.user && populated && populated.bulletins) {
+        //     context.result.user = {
+        //       ...context.result.user,
+        //       bulletins: populated.bulletins,
+        //     };
+        //   }
         if (context.result.authentication) delete context.result.authentication;
-
-        if (context.result.user && populated && populated.bulletins) {
-          console.log('populated ----------------', populated);
-          context.result.user = {
-            ...context.result.user,
-            bulletins: populated.bulletins,
-          };
-        }
         return context;
       },
     ],
--- back/src/services/bulletins/bulletins.hooks.js
+++ back/src/services/bulletins/bulletins.hooks.js
@@ -1,11 +1,19 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const decode = require('jwt-decode');
 
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [authenticate('jwt')],
+    create: [
+      async (context) => {
+        authenticate('jwt');
+        const userId = decode(context.params.authentication.accessToken).sub;
+        context.data.user = userId;
+        return context;
+      },
+    ],
     update: [authenticate('jwt')],
     patch: [authenticate('jwt')],
     remove: [authenticate('jwt')],
@@ -13,15 +21,25 @@ module.exports = {
 
   after: {
     all: [],
-    find: [],
+    find: [
+      async (context) => {
+        const bulletModel = context.app.service('bulletins').Model;
+        const populated = await bulletModel
+          .find({})
+          .populate('user', { username: 1 });
+        context.result = populated;
+        return context;
+      },
+    ],
     get: [],
     create: [
       async (context) => {
         const UserModel = context.app.service('users').Model;
+        const userId = decode(context.params.authentication.accessToken).sub;
         const user = await UserModel.findOne({
-          _id: context.params.user._id,
+          _id: userId,
         });
-        if (!user) throw new Error('User not found');
+        if (!user) throw new Error('No user found :(');
         user.bulletins = user.bulletins.concat(context.result._id);
         await user.save();
       },
