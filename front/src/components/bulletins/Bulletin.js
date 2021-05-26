5156a9517eb7119e71e170a3916582fe7784f224
p3-Careeria <petri.laine@student.careeria.fi>
2021-05-26 15:04:33 +0300

    accordion alku

--- front/src/components/bulletins/Bulletin.js
+++ front/src/components/bulletins/Bulletin.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Bulletin = ({ bulletin }) => {
+const Bulletin = ({ bulletin, index, indexCount, handleClick, active }) => {
   return (
-    <div>
-      <strong> {bulletin.header}</strong>
-      <p>{bulletin.description}</p>
-      <br />
-    </div>
+    <>
+      <h3>
+        <button
+          type="button"
+          onClick={(e) => handleClick(e, index)}
+          className={active === index ? 'accordionBtn active' : 'accordionBtn inactive'}
+          aria-expanded={active === index ? 'true' : 'false'}
+          aria-controls={`sect-${indexCount(index)}`}
+          aria-disabled={active === index ? 'true' : 'false'}
+          tabIndex={indexCount(index)}
+        >
+          <span className="accordion-title-wrapper">
+            {bulletin.header}
+            {/* <span className={active === index ? 'plus' : 'minus'} /> */}
+          </span>
+        </button>
+      </h3>
+      <div
+        id={`sect-${indexCount(index)}`}
+        className={active === index ? 'panel-open' : 'panel-close'}
+      >
+        {bulletin.description}
+      </div>
+    </>
   );
 };
 
@@ -16,6 +35,10 @@ Bulletin.propTypes = {
     header: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
   }).isRequired,
+  index: PropTypes.number.isRequired,
+  indexCount: PropTypes.func.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  active: PropTypes.number.isRequired,
 };
 
 export default Bulletin;
--- front/src/components/bulletins/index.js
+++ front/src/components/bulletins/index.js
@@ -1,30 +1,51 @@
 /* eslint-disable no-underscore-dangle */
-import React from 'react';
+import React, { useState } from 'react';
 import { Text } from '@react-md/typography';
 import { useSelector } from 'react-redux';
 import Bulletin from './Bulletin';
 
 const Bulletins = () => {
   const bulletins = useSelector((x) => x.bulletinsState.bulletins);
+  const [active, setActive] = useState(-1);
+  let indexPlus;
+
+  const handleClick = (e, index) => {
+    e.preventDefault();
+    if (active === index) setActive(-1);
+    else setActive(index);
+  };
+  const indexCount = (index) => {
+    indexPlus = index + 1;
+    return indexPlus;
+  };
+
   return (
     <>
       <div className="centercenter">
         <div className="headerCard">
           <Text type="headline-4">Bulletin board</Text>
-          <Text type="body-1">This will be where you can see the bulletins.</Text>
+          <Text type="body-1">
+            This will be where you can see the bulletins and perhaps filter them with category etc.
+          </Text>
         </div>
         <br />
       </div>
       <br />
-      <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '50px' }}>
+      <form style={{ backgroundColor: 'pink' }}>
         {bulletins
           .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-          .map((b) => (
-            <div key={b._id}>
-              <Bulletin bulletin={b} />
+          .map((b, i) => (
+            <div key={b._id} className="accordionWrap">
+              <Bulletin
+                bulletin={b}
+                index={i}
+                handleClick={handleClick}
+                indexCount={indexCount}
+                active={active}
+              />
             </div>
           ))}
-      </div>
+      </form>
     </>
   );
 };
